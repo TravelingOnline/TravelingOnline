e9ef062678c61f250829c9f753b5adf440452739
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-23 16:33:02 +0330

    feat: impl update credit card

--- bank/internal/bank/port/credit_card.go
+++ bank/internal/bank/port/credit_card.go
@@ -16,7 +16,8 @@ var (
 type CreditCardRepo interface {
 	CreateCardAndAddToWallet(ctx context.Context, creditCard *domain.CreditCard, userID uuid.UUID) (*domain.CreditCard, error)
 	GetUserWalletCards(ctx context.Context, userID uuid.UUID) ([]domain.CreditCard, error)
-	// UpdateCard(ctx context.Context,creditCard *domain.CreditCard)(*domain.CreditCard)
+	UpdateCard(ctx context.Context, creditCard *domain.CreditCard) error
+	DeleteCard(ctx context.Context, creditCardId uuid.UUID) error
 }
 
 func NewCreditCard(number string) *domain.CreditCard {
