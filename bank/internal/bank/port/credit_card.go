b2ed7ad8fb76000168dcf269b1622d5fee2550ca
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-23 16:04:41 +0330

    feat: add creaditcard repo in port package

---
+++ bank/internal/bank/port/credit_card.go
@@ -0,0 +1,26 @@
+package port
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/onlineTraveling/bank/internal/bank/domain"
+)
+
+var (
+	ErrInvalidCardNumber = errors.New("invalid card number")
+	ErrCardAlreadyExists = errors.New("card already exists in wallet")
+)
+
+type CreditCardRepo interface {
+	CreateCardAndAddToWallet(ctx context.Context, creditCard *domain.CreditCard, userID uuid.UUID) (*domain.CreditCard, error)
+	GetUserWalletCards(ctx context.Context, userID uuid.UUID) ([]domain.CreditCard, error)
+	// UpdateCard(ctx context.Context,creditCard *domain.CreditCard)(*domain.CreditCard)
+}
+
+func NewCreditCard(number string) *domain.CreditCard {
+	return &domain.CreditCard{
+		Number: number,
+	}
+}
