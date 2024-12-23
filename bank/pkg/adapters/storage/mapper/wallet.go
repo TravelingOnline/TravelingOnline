c1f06196d6893dab189013d728586ff8c18fa188
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-23 16:09:22 +0330

    feat: impl mapper for wallet and bank models

---
+++ bank/pkg/adapters/storage/mapper/wallet.go
@@ -0,0 +1,71 @@
+package mapper
+
+import (
+	"github.com/onlineTraveling/bank/internal/bank/domain"
+	"github.com/onlineTraveling/bank/pkg/adapters/storage/types"
+)
+
+func WalletEntityToDomain(entity *types.Wallet) *domain.Wallet {
+	return &domain.Wallet{
+		ID:      &entity.ID,
+		UserID:  *entity.UserID,
+		Balance: entity.Balance,
+	}
+}
+
+func WalletDomainToEntity(domainWallet *domain.Wallet) *types.Wallet {
+	return &types.Wallet{
+		UserID:  &domainWallet.UserID,
+		Balance: domainWallet.Balance,
+	}
+}
+
+func CreditCardEntityToDomain(entity *types.CreditCard) *domain.CreditCard {
+	return &domain.CreditCard{
+		ID:     entity.ID,
+		Number: entity.Number,
+	}
+}
+
+func BatchCreditCardEntityToDomain(entities []*types.CreditCard) []domain.CreditCard {
+	var domainCreditCards []domain.CreditCard
+	for _, e := range entities {
+		domainCreditCards = append(domainCreditCards, domain.CreditCard{ID: e.ID, Number: e.Number})
+	}
+	return domainCreditCards
+}
+
+func CreditCardDomainToEntity(domainWallet *domain.CreditCard) *types.CreditCard {
+	return &types.CreditCard{
+		Number: domainWallet.Number,
+	}
+}
+
+func DomainTransactionToTransactionEntity(domainTr *domain.BankTransaction) *types.BankTransaction {
+	var toWl *types.Wallet
+	fromWl := WalletDomainToEntity(domainTr.FromWallet)
+	if !domainTr.IsPaidToSystem {
+		toWl = WalletDomainToEntity(domainTr.ToWallet)
+	}
+	return &types.BankTransaction{
+		Amount:         domainTr.Amount,
+		FromWallet:     fromWl,
+		ToWallet:       toWl,
+		IsPaidToSystem: domainTr.IsPaidToSystem,
+	}
+}
+
+func TransactionEntityToDomain(entity *types.BankTransaction) *domain.BankTransaction {
+	var toWalDomain *domain.Wallet
+	fromWalDomain := WalletEntityToDomain(entity.FromWallet)
+	if !entity.IsPaidToSystem {
+		toWalDomain = WalletEntityToDomain(entity.ToWallet)
+	}
+	return &domain.BankTransaction{
+		Amount:         entity.Amount,
+		Status:         entity.Status,
+		FromWallet:     fromWalDomain,
+		ToWallet:       toWalDomain,
+		IsPaidToSystem: entity.IsPaidToSystem,
+	}
+}
