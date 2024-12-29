ea5df46ad7f0108fe2e6309932a9989aeee661de
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-12-29 18:41:48 +0330

    feat: add isAdmin and superadmin and isBlocked fields to user  model

--- auth/pkg/adapters/storage/types/user.go
+++ auth/pkg/adapters/storage/types/user.go
@@ -8,4 +8,31 @@ type User struct {
 	gorm.Model
 	Email        string `gorm:"column:email;unique;not null"`
 	PasswordHash string `gorm:"column:password_hash;not null"`
+	IsSuperAdmin bool   `gorm:"not null; default:false"`
+	IsAdmin      bool   `gorm:"not null; default:false"`
+	//Roles        []Role         `gorm:"many2many:user_roles;"`
+	IsBlocked bool `gorm:"not null; default:false"`
 }
+
+//type Role struct {
+//	gorm.Model
+//	Name        string `gorm:"uniqueIndex;not null"`
+//	Description string
+//	Permissions []Permission `gorm:"many2many:role_permissions;"`
+//}
+
+//type Permission struct {
+//	gorm.Model
+//	Name        string `gorm:"uniqueIndex;not null"`
+//	Description string
+//}
+//
+//type UserRole struct {
+//	UserID uuid.UUID `gorm:"type:uuid;not null"`
+//	RoleID uint      `gorm:"primaryKey"`
+//}
+//
+//type RolePermission struct {
+//	RoleID       uint `gorm:"primaryKey"`
+//	PermissionID uint `gorm:"primaryKey"`
+//}
